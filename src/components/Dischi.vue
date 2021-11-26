f2905282d63f0ffadb2b60d918380a122eb98537
Dani9898 <danieleoliva98@gmail.com>
2021-11-26 13:30:01 +0100

    creazione componenti e import axios

---
+++ src/components/Dischi.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+   
+  </div>
+</template>
+
+<script>
+
+import axios from "axios"
+
+export default {
+  name: 'Dischi',
+  data(){
+      return {
+          apiURL: "https://flynn.boolean.careers/exercises/api/array/music",
+          listaDischi = []
+      }
+  },
+
+  created(){
+      this.getDischi()
+  },
+
+  methods:{
+
+      getDischi(){
+        axios
+        .get(this.apiURL)
+        .then((result) => {
+            this.listaDischi = result.data;
+        })        
+      }
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/Disco.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="my-disk">
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Disco',
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
